850bb088009af3d7e4a1a71cfafa1836e0a72d7f
KSHyun9692 <KSHyun9692@gmail.com>
2025-10-28 22:00:26 +0900

    mvc/test commit

---
+++ hello-spring/src/main/java/com/hello/hello_spring/service/MemberService.java
@@ -0,0 +1,51 @@
+package com.hello.hello_spring.service;
+
+
+import com.hello.hello_spring.domain.Member;
+import com.hello.hello_spring.repository.MemberRepository;
+import com.hello.hello_spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     * @param member
+     * @return id
+     */
+    public Long join(Member member){
+        //같은 이름 중복안되도록
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName( member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원명 입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     * @return
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    /**
+     * id조회
+     * @param memberId
+     * @return
+     */
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
