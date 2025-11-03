625d1c8769b9122147bfc95acd662f56a71c66c0
KSHyun9692 <KSHyun9692@gmail.com>
2025-11-03 20:43:22 +0900

    Jpa Test

--- hello-spring/src/main/java/com/hello/hello_spring/SpringConfig.java
+++ hello-spring/src/main/java/com/hello/hello_spring/SpringConfig.java
@@ -1,24 +1,35 @@
 package com.hello.hello_spring;
 
+import com.hello.hello_spring.repository.JpaMemberRepository;
 import com.hello.hello_spring.repository.MemberRepository;
 import com.hello.hello_spring.repository.MemoryMemberRepository;
 import com.hello.hello_spring.service.MemberService;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 //Configuration을 통해 java로 직접 bean 주입
-//@Configuration
-//public class SpringConfig {
-//
-//    @Bean
-//    public MemberService memberService() {
-//
-//        return new MemberService(memberRepository());
-//    }
-//
-//    @Bean
-//    public MemberRepository memberRepository() {
-//
-//        return new MemoryMemberRepository();
-//    }
-//}
+@Configuration
+public class SpringConfig {
+
+    private EntityManager em;
+
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }
+
+
+    @Bean
+    public MemberService memberService() {
+
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+
+        return new JpaMemberRepository(em);
+    }
+}
--- hello-spring/src/main/java/com/hello/hello_spring/controller/HomeController.java
+++ hello-spring/src/main/java/com/hello/hello_spring/controller/HomeController.java
@@ -3,7 +3,7 @@ package com.hello.hello_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+//@Controller
 public class HomeController {
 
     @GetMapping("/")
